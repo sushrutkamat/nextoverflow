d7a7ab359ac81c708edbf75672e0b3e9d6eb582b
Sushrut Kamat <38829890+sushrutkamat@users.noreply.github.com>
2024-11-19 18:14:16 +0530

    Eslint & Prettier set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl text-white font-bold">
+    <h1 className="text-3xl font-bold text-white">
       Welcome to World of NextJs
     </h1>
   );
